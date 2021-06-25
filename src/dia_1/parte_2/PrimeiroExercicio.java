4e455fa7153c1e76bb40e1906f089eea4486851e
Arthur Massaini de Santana <massaini.arthur@gmail.com>
2021-06-25 11:26:29 -0300

    Dia 1 - Exercicios parte 1 e parte 2 - manha

---
+++ src/dia_1/parte_2/PrimeiroExercicio.java
@@ -0,0 +1,18 @@
+package dia_1.parte_2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class PrimeiroExercicio {
+    public static void main(String[] args) {
+        Integer array[] = {52, 10, 85, 1324, 01, 13, 62, 30, 12, 127};
+
+        Arrays.sort(array);
+        System.out.println("Ordenação crescente do array:");
+        System.out.println(Arrays.toString(array));
+
+        Arrays.sort(array, Collections.reverseOrder());
+        System.out.println("Ordenação decrescente do array:");
+        System.out.println(Arrays.toString(array));
+    }
+}
